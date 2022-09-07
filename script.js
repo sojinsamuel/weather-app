f8e57d1bda0ce680ac7d35b96bc311fdf2b68d80
Sam <77185816+sojinsamuel@users.noreply.github.com>
2022-09-08 00:00:45 +0530

    Exposed API Key Found
    Exposing your personal API key to public can create critical issues

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let weather = {
-    "apikey": "76c28839ec6cbca0287b0e124c3dc6f1",
+    "apikey": "Your API Key",
     fetchWeather: function (){
         fetch("https://api.openweathermap.org/data/2.5/weather?q=Pune&units=metric&appid=76c28839ec6cbca0287b0e124c3dc6f1")
     }
